13f72a02450f296a9550c6b79100e2b95bc62534
Sabreen Hasan <sabreen_hasan@yahoo.com>
2018-11-06 21:06:59 -0500

    its a mess smh

--- fifteen.js
+++ fifteen.js
@@ -96,63 +96,131 @@ function naturalBorder() {
 }
 
 function backgroundSelector() {
+//Adds functionality to drop down list of background choices.
+    var selection = document.getElementById("backgrounds");
+    selection.onchange = puzzleBackground;
+}
 
+function puzzleBackground() {
+//Adding a background to each piece of the puzzle.
+    var backG = document.getElementById("backgrounds").value;
+    var piecesDem = document.querySelectorAll(".puzzlepiece");
+
+    for (var m = 0; m < piecesDem.length; m++) {
+        piecesDem[m].style.backgroundImage = "url(" + backG + ")";        
+    }
+    locally["backgrounds"] = backG;
+}
+
+function highlightedPiece(thisPiece) {
+//Changes the features of a movable piece.
+    highlight = document.getElement("moveablepiece") 
+    thisPiece.value = true;
 }
 
 function shuffleButton() {
-    var button = getElementById("shufflebutton");
-    button.addEventListener("click", shuffleGame);
+//Adds functionality to the shuffle button
+    var button = document.getElementById("shufflebutton");
+    button.onclick, shuffleGame;
 }
+
+function shuffleGame() {
+//Shuffle the board pieces
+    document.getElementById("winnerDetermined").innerHTML = ""; //clears the "you win" text from screen. 
+
+    solvable = false;
+    var piecesDem = document.getElementsByClassName("puzzlepiece");
+
+    for (m = 0; m < 1000; m++) {
+    //Searches for movable pieces and randomly chooses one movable piece to put into the empty space.
+        var spacesBeside = [];
+        for (var n = 0; n < piecesDem.length; n++) {
+            if (tryT(piecesDem[n]) || tryU(piecesDem[n])) {
+                spacesBeside.push(pieces[n]);
+            }
+        }
+        var sumn = Math.floor(Math.random() * spacesBeside.length);
+
+        spacesBeside[sumn].value = true;
+        movePiece(spacesBeside[sumn]);
+        spacesBeside[sumn].value = false;
+    }
+
+    solvable = true;
+}   
+
+function checkSolved(){
+//Checks the completeness of the puzzles.
+    var solveT = 0;
+    var solveU = 0;
+    var count = 0;
+    var finish = 0;
+    var piecesDem = document.getElementsByClassName("puzzlepiece");
+
+    for (var m = 0; m < piecesDem.length; m++) {
+    //skips through the spaces to compare the coordinates to the current positions of the puzzle pieces.
+        if (count == puzzleRows_Columns) {
+        //If coordinates match then completed counter is increased by one, or if not then it is reset to zero.
+            solveT = 0;
+            solveU += sizeForPiece;
+            count = 0;
+        }    
+        if (parseInt(piecesDem[m].style.left) == solveT && parseInt(piecesDem[m].style.top) == solveU) {
+            finish++;
+        }
+        else {
+            finish = 0;
+        }
+        count++;
+        solveT += sizeForPiece;
+    }
+
+    if (finish == sizeForPiece) {
+    //if the puzzle is complete then the "you won" message is displayed and the number of wins incremented by one.
+        document.getElementById("winnings").innerHTML = "Raaaaaae!! You WON!!";
+        var wins = locally["winsSoFar"] = wins;
+        winnings;
+    } 
+    else {
+    //Win message is cleared from the screen
+    document.getElementById("winsSoFar").innerHTML = "";
+    }   
+}
+
+function tryT(thisPiece) {
+    if (parseInt(thisPiece.style.left) == emptyT) {
+        if (parseInt(thisPiece.style.top) == (emptyU - sizeForPiece) || parseInt(thisPiece.style.top) == (emptyU + sizeForPiece)) {
+            return true;
+        }
+    }
+}
+
+function tryU(thisPiece) {
+    if (parseInt(thisPiece.style.top) == emptyU) {
+        if (parseInt(thisPiece.style.left) == (emptyT - sizeForPiece) || parseInt(thisPiece.style.left) == (emptyT + sizeForPiece)) {
+            return true;
+        }
+    }
+}
+
+function returnBackground() {
+//function to determine the user's selected background.
+    if(locally["backgrounds"]) {
+        var backG = document.getElementById("backgrounds");
+        backG.value = locally["backgrounds"];
+
+        var piecesDem = document.querySelectorAll(".puzzlepiece");
+        for (var m = 0; m < piecesDem.length; m++) {
+            piecesDem[m].style.backgroundImage = "url(" + backG.value + ")";
+        }
+    }
+} 
 
 function winnings() {
 //Determines the numnber of wins so far and returns that amount
     var wins = document.getElementById("winsSoFar");
-    if (!["winsSoFar"]) {
-        ["winsSoFar"] = 0;
+    if (!locally["winsSoFar"]) {
+        locally["winsSoFar"] = 0;
     }
-    wins.innerHTML = "You have won " + ["winsSoFar"];
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function () {
-    
-    image.addEventListener("load", onImage);
-    image.src = "cj-face.jpeg"
-}
-
-function onImage() {
-    puzzleWidth = Math.floor(_image.width / PUZZLE_DIFFICULTY)
-    puzzleHeight = Math.floor(_image.height / PUZZLE_DIFFICULTY)
-    puzzleWidth = _pieceWidth * PUZZLE_DIFFICULTY;
-    puzzleHeight = _pieceHeight * PUZZLE_DIFFICULTY;
-}
+    wins.innerHTML = "Total wins: " + locally["winsSoFar"];
+}();
